72f4093b338f2ddde4a2d445489cb9bf99148ab8
rituuu001 <adhikaritu806@gmail.com>
2026-03-13 21:10:52 +0545

    DESIGNED THE LOADING SCREEN

--- include/common.h
+++ include/common.h
@@ -7,6 +7,14 @@
 #define BG     (Color){26, 32, 44, 255}
 #define LIGHT  (Color){180, 200, 200, 60}
 #define WRONG_COLOR   (Color){255, 80, 80, 255}   // red for wrong
+typedef enum
+{
+    SCREEN_LOAD,
+    SCREEN_MODE,
+    SCREEN_TYPING,
+    SCREEN_GAMEOVER,
+    SCREEN_LEADERBOARD
+}GAMESCREEN;
 #endif
 
 // rest of your structs...
